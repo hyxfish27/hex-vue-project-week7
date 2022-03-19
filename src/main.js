70f5558af53b76f662d114aaf0a98e017ee6939f
hyxfish27 <avicky1515@gmail.com>
2022-03-19 20:25:50 +0800

    initial enviroment

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
+// 用來載入需要用在整個專案的套件
+// 1. bootstrap 2. axios 3. VueAxios
 import { createApp } from 'vue'
+import 'bootstrap'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(VueAxios, axios)
+app.mount('#app')
